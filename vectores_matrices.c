e96dd26f8fe9bffe510f82b97f5a17c2da3e4ec5
Matheo Cañarejo <matheo.canarejo@udla.edu.ec>
2026-05-24 22:53:32 -0500

    Update vectores_matrices.c
    Se separa todo el código en funciones organizadas

--- vectores_matrices.c
+++ vectores_matrices.c
@@ -1,12 +1,8 @@
 #include <stdio.h>
 
-int main() {
+void ingresarNotas(float (*notas)[3], int cantidadEstudiantes) {
     int i, j;
-    float notas[7][3];
-    float suma;
-
-    // Ingreso de notas con validación
-    for(i = 0; i < 7; i++) {
+    for(i = 0; i < cantidadEstudiantes; i++) {
         printf("\nEstudiante %d:\n", i + 1);
         for(j = 0; j < 3; j++) {
             do {
@@ -15,70 +11,78 @@ int main() {
             } while(notas[i][j] < 0 || notas[i][j] > 10);
         }
     }
+}
 
     // Promedio por estudiante
+    void promedioEstudiantes(float (*notas)[3], int cantidadEstudiantes) {
+    int i, j;
+    float suma;
     printf("\n--- PROMEDIO POR ESTUDIANTE ---\n");
-    for(i = 0; i < 7; i++) {
+    for(i = 0; i < cantidadEstudiantes; i++) {
         suma = 0;
         for(j = 0; j < 3; j++) {
             suma += notas[i][j];
         }
         printf("Estudiante %d: %.2f\n", i + 1, suma / 3);
     }
+}
 
     // Promedio por asignatura
+    void promedioAsignaturas(float (*notas)[3], int cantidadEstudiantes) {
+    int i, j;
+    float suma;
     printf("\n--- PROMEDIO POR ASIGNATURA ---\n");
     for(j = 0; j < 3; j++) {
         suma = 0;
-        for(i = 0; i < 7; i++) {
+        for(i = 0; i < cantidadEstudiantes; i++) {
             suma += notas[i][j];
         }
-        printf("Asignatura %d: %.2f\n", j + 1, suma / 7);
+        printf("Asignatura %d: %.2f\n", j + 1, suma / cantidadEstudiantes);
     }
+}
 
     // Máximo y mínimo por estudiante
+    void maxMinEstudiante(float (*notas)[3], int cantidadEstudiantes) {
+    int i, j;
     printf("\n--- NOTA MAX Y MIN POR ESTUDIANTE ---\n");
-    for(i = 0; i < 7; i++) {
+    for(i = 0; i < cantidadEstudiantes; i++) {
         float max = notas[i][0];
         float min = notas[i][0];
-
         for(j = 1; j < 3; j++) {
             if(notas[i][j] > max) max = notas[i][j];
             if(notas[i][j] < min) min = notas[i][j];
         }
-
         printf("Estudiante %d -> Max: %.2f | Min: %.2f\n", i + 1, max, min);
     }
 
+
     // Máximo y mínimo por asignatura
+    void maxMinAsignatura(float (*notas)[3], int cantidadEstudiantes) {
+    int i, j;
     printf("\n--- NOTA MAX Y MIN POR ASIGNATURA ---\n");
     for(j = 0; j < 3; j++) {
         float max = notas[0][j];
         float min = notas[0][j];
-
-        for(i = 1; i < 7; i++) {
+        for(i = 1; i < cantidadEstudiantes; i++) {
             if(notas[i][j] > max) max = notas[i][j];
             if(notas[i][j] < min) min = notas[i][j];
         }
-
         printf("Asignatura %d -> Max: %.2f | Min: %.2f\n", j + 1, max, min);
     }
+}
 
     // Aprobados y reprobados
+    void aprobacion(float (*notas)[3], int cantidadEstudiantes) {
+    int i, j;
     printf("\n--- APROBADOS Y REPROBADOS ---\n");
     for(j = 0; j < 3; j++) {
         int aprobados = 0, reprobados = 0;
-
-        for(i = 0; i < 7; i++) {
+        for(i = 0; i < cantidadEstudiantes; i++) {
             if(notas[i][j] >= 6)
                 aprobados++;
             else
                 reprobados++;
         }
-
-        printf("Asignatura %d -> Aprobados: %d | Reprobados: %d\n",
-               j + 1, aprobados, reprobados);
+        printf("Asignatura %d -> Aprobados: %d | Reprobados: %d\n", j + 1, aprobados, reprobados);
     }
-
-    return 0;
 }
